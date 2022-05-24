4202c4079ba0ef0b1d91f6dfa989f6cc55ee79f1
yash-agrwl <agarwalyash7012@gmail.com>
2022-05-24 22:30:50 +0530

    added UC9: Ability to create employeePayroll data and add fields as required

---
+++ js/EmployeePayrollData.js
@@ -0,0 +1,51 @@
+class EmployeePayrollData {
+
+    // Getter and Setter method.
+    get id() { return this._id; }
+    set id(id) {
+        this._id = id;
+    }
+
+    get name() { return this._name }
+    set name(name) {
+        this._name = name;
+    }
+
+    get ProfilePic() { return this._ProfilePic; }
+    set ProfilePic(profilePic) {
+        this._ProfilePic = profilePic;
+    }
+
+    get gender() { return this._gender; }
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get department() { return this._department; }
+    set department(department) {
+        this._department = department;
+    }
+
+    get salary() { return this._salary; }
+    set salary(salary) {
+        this._salary = salary;
+    }
+
+    get note() { return this._note; }
+    set note(note) {
+        this._note = note;
+    }
+
+    get startDate() { return this._startDate; }
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
+
+    // Method.
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "id=" + this.id + ", name=" + this.name + ", gender=" + this.gender + ", profilePic=" + this.ProfilePic
+            + ", department=" + this.department + ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
+    }
+}
