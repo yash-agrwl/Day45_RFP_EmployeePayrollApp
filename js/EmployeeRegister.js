772e660dc9a2d9f90ccc40350450be1739163374
yash-agrwl <agarwalyash7012@gmail.com>
2022-05-25 13:48:48 +0530

    added UC10A: Ability to set event listener when document is loaded on Salary Range so as to display appropriate value

--- js/EmployeeRegister.js
+++ js/EmployeeRegister.js
@@ -1,6 +1,9 @@
-// UC8 - Update Salary value.
-let salary = document.querySelector("#salary");
-let output = document.querySelector(".salary-output");
-salary.addEventListener("input", function () {
-    output.textContent = salary.value;
-});
+// UC10
+window.addEventListener('DOMContentLoaded', (event) => {
+    // UC10A - Update Salary value.
+    let salary = document.querySelector("#salary");
+    let output = document.querySelector(".salary-output");
+    salary.addEventListener("input", function () {
+        output.textContent = salary.value;
+    });
+})
